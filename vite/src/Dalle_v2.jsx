f1494ec59fb38a06b8697932ef598981ca8dc8de
reng <reng@ultracombos.com>
2025-06-30 11:02:13 +0800

    fix layout

--- vite/src/Dalle_v2.jsx
+++ vite/src/Dalle_v2.jsx
@@ -223,13 +223,22 @@ function DalleV2() {
 
     
   }
-  
+  const clearInput=()=>{
+    for(var i=0;i<5;++i){
+      let el=document.querySelector(`#_answer_${i+1}`);
+      if(el) el.value='';
+    }
+  }
+
   const restart=()=>{
     setStatus(STATUS.IDLE);
     setButtons();
     setMessageId();
     setImageSrc();
     // refInput.current.value="";
+
+    clearInput();
+
 
     checkTimeout();
   }
@@ -277,9 +286,9 @@ function DalleV2() {
 
   return (
     <div className="main v2">
-        <button className="absolute top-[3rem] left-[2.88rem] cbutton" onClick={restart}>{lang=="en"? "restart":( lang=='zh'?'重新整理':"Herstarten")}</button>        
+        <button className="absolute top-[3rem] left-[1.87rem] cbutton" onClick={restart}>{lang=="en"? "restart":( lang=='zh'?'重新整理':"Herstarten")}</button>        
         
-        <div className='absolute top-[3rem] right-[2.88rem] flex flex-row gap-[4px] font-bold text-[1rem]'>
+        <div className='absolute top-[3rem] right-[1.87rem] flex flex-row gap-[4px] font-bold text-[1rem]'>
         <div onClick={()=>navigate(`/v2/zh${auto?`/${auto}`:''}`)} 
             className={`${lang=='zh'? 'underline':''} cursor-pointer`}>中文</div>
           /
@@ -295,22 +304,25 @@ function DalleV2() {
           <div className='w-full flex flex-col justify-center items-center gap-2 bg-back p-[0.6rem]'>
             <div className='w-full aspect-square flex justify-center items-center border-[1.5px] border-[rgba(255,255,255,0.6)]'>
               {!imageSrc? (
-                <div className='w-full whitespace-nowrap flex justify-center text-[2rem] font-bold'>{lang=='en'? TITLE: (lang=='zh'?TITLE_ZH: TITLE_NL)}</div>
+                <div className='w-full whitespace-nowrap flex justify-center text-[2rem] font-bold'>{lang=='en'? 'Inner Alien Generator': (lang=='zh'?'內在外星人生成系統': 'Inner Alien Generator')}</div>
               ):(
                 imageSrc.map((src, index)=><img key={index} src={src}/>)                          
               )}  
             </div>          
           </div>
           
+          <div className='footer'>
+            Powered by DALL·E
+          </div>
         </div>
         <Album ref={refAlbum} tmp={imageSrc} lang={lang}  
           onSelect={()=>{
             setAutorun(false);
             if(status==STATUS.IDLE) checkTimeout();
           }}/>
-        <div className='footer'>
+        {/* <div className='footer'>
           Powered by DALL·E
-        </div>
+        </div> */}
     </div>
   )
 }
--- vite/src/comps/manualV2.jsx
+++ vite/src/comps/manualV2.jsx
@@ -42,39 +42,43 @@ const getButtonText=(status, lang)=>{
 }
 
 export const ManualV2=({status, onSend})=>{
+    
+    const __h4_style="";
+    const __input_style="flex-1";
+    const __section_style="flex-1 text-[0.9375rem]";
 
     return (
         <div className={__style}>
             <section>
-                <h3 className="text-[1.25rem]">How would you describe your inner alien?</h3>
-                <p className="text-[0.75rem]">After listening to the guided meditation, please respond to the following questions and click the “Generate” button to create your inner alien.
+                <h3 className="text-[1.25rem] tracking-[0.025rem] font-bold">How would you describe your inner alien?</h3>
+                <p className="text-[0.6875rem] tracking-[0.015rem]">After listening to the guided meditation, please respond to the following questions and click the “Generate” button to create your inner alien.<br/>
                 *If a question does not apply to your inner alien, you may leave it blank.
                 </p>
             </section>
 
-            <section>
+            <section className={__section_style}>
                 <h4>1. What kind of shape and features does your inner alien have?</h4>
-                <textarea id="_answer_1" placeholder="Enter your thoughts..."></textarea>
+                <textarea id="_answer_1" placeholder="Enter your thoughts..." className={__input_style}></textarea>
             </section>
 
-            <section>
+            <section className={__section_style}>
                 <h4>2. What color and texture does your inner alien have?</h4>
-                <textarea id="_answer_2" placeholder="Enter your thoughts..."></textarea>
+                <textarea id="_answer_2" placeholder="Enter your thoughts..." className={__input_style}></textarea>
             </section>
 
-            <section>
+            <section className={__section_style}>
                 <h4>3. What kind of behavior or movement does your inner alien exhibit?</h4>
-                <textarea id="_answer_3" placeholder="Enter your thoughts..."></textarea>
+                <textarea id="_answer_3" placeholder="Enter your thoughts..."  className={__input_style}></textarea>
             </section>
 
-            <section>
+            <section className={__section_style}>
                 <h4>4. How does your inner alien communicate with you?</h4>
-                <textarea id="_answer_4" placeholder="Enter your thoughts..."></textarea>
+                <textarea id="_answer_4" placeholder="Enter your thoughts..."  className={__input_style}></textarea>
             </section>
 
-            <section>
+            <section className={__section_style}>
                 <h4>5. What kind of emotions or feelings does your inner alien have?</h4>
-                <textarea id="_answer_5" placeholder="Enter your thoughts..."></textarea>
+                <textarea id="_answer_5" placeholder="Enter your thoughts..."  className={__input_style}></textarea>
             </section>
 
             <button className={`cbutton self-center ${getButtonColor(status)}`} 
@@ -86,40 +90,45 @@ export const ManualV2=({status, onSend})=>{
 
 export const ManualZHV2=({status, onSend})=>{
 
+    const __h4_style="";
+    const __input_style="flex-1";
+    const __section_style="flex-1";
+
     return (
         <div className={__style}>
             <section>
-                <h3 className="text-[1.25rem]">你會如何描述你的內在外星人呢？</h3>
-                <p className="text-[0.75rem]">在聆聽冥想引導過後，回答以下相應的問題，並點選「開始生成」按鈕，生成你內心的外星人。
+                <h3 className="text-[1.25rem] tracking-[0.025rem] font-bold">你會如何描述你的內在外星人呢？</h3>
+                <p className="text-[0.75rem] leading-[1.375rem] tracking-[0.015rem]">在聆聽冥想引導過後，回答以下相應的問題，並點選「開始生成」按鈕，生成你內心的外星人。<br/>
                 *若有題目不適用於你的內在外星人的描述則無需填寫</p>
             </section>
 
-            <section>
-                <h4>1. 你的內在外星人擁有什麼樣的形體和特徵呢？</h4>
-                <textarea id="_answer_1" placeholder="輸入你的想法..."></textarea>
+            <section className={__section_style}>
+                <h4 className={__h4_style}>1. 你的內在外星人擁有什麼樣的形體和特徵呢？</h4>
+                <textarea id="_answer_1" placeholder="輸入你的想法..." className={__input_style}></textarea>
             </section>
 
-            <section>
-                <h4>2. 你的內在外星人是什麼顏色和質地呢？</h4>
-                <textarea id="_answer_2" placeholder="輸入你的想法..."></textarea>
+            <section className={__section_style}>
+                <h4 className={__h4_style}>2. 你的內在外星人是什麼顏色和質地呢？</h4>
+                <textarea id="_answer_2" placeholder="輸入你的想法..." className={__input_style}></textarea>
             </section>
 
-            <section>
-                <h4>3. 你的內在外星人有什麼行為或是怎麼移動的呢</h4>
-                <textarea id="_answer_3" placeholder="輸入你的想法..."></textarea>
+            <section className={__section_style}>
+                <h4 className={__h4_style}>3. 你的內在外星人有什麼行為或是怎麼移動的呢</h4>
+                <textarea id="_answer_3" placeholder="輸入你的想法..." className={__input_style}></textarea>
             </section>
 
-            <section>
-                <h4>4. 你的內在外星人是如何與你溝通的呢？</h4>
-                <textarea id="_answer_4" placeholder="輸入你的想法..."></textarea>
+            <section className={__section_style}>
+                <h4 className={__h4_style}>4. 你的內在外星人是如何與你溝通的呢？</h4>
+                <textarea id="_answer_4" placeholder="輸入你的想法..." className={__input_style}></textarea>
             </section>
 
-            <section>
-                <h4>5. 你的內在外星人有什麼樣的情緒和感受呢？</h4>
-                <textarea id="_answer_5" placeholder="輸入你的想法..."></textarea>
+            <section className={__section_style}>
+                <h4 className={__h4_style}>5. 你的內在外星人有什麼樣的情緒和感受呢？</h4>
+                <textarea id="_answer_5" placeholder="輸入你的想法..." className={__input_style}></textarea>
             </section>
 
-            <button className="cbutton bg-green self-center" onClick={onSend}>{getButtonText(status, 'zh')}</button>
+            <button className={`cbutton self-center ${getButtonColor(status)}`} 
+                    onClick={onSend}>{getButtonText(status, 'zh')}</button>
         </div>
     )
 
